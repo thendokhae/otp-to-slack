66416c63a90e11f91980e2ac175cc73ecd011f15
thendokhae <thendokhae18@gmail.com>
2019-02-11 09:34:33 +0200

    added slack message sending intergration

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,4 +1,5 @@
 import { IonicModule } from '@ionic/angular';
+import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -10,6 +11,7 @@ import { Tab1Page } from './tab1.page';
     IonicModule,
     CommonModule,
     FormsModule,
+    HttpModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
